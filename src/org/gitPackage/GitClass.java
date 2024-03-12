87ccd8203055900ddbf50f198ef54e21a4da512f
KevinJohnsonR <kevinjohnsonr1996@gmail.com>
2024-03-12 16:52:46 +0530

    done

--- src/org/gitPackage/GitClass.java
+++ src/org/gitPackage/GitClass.java
@@ -5,6 +5,9 @@ public class GitClass {
 	
 	public static void main(String[] args) {
 		System.err.println("hello");
+		
+		System.out.println("helo java");
+		
 	}
 
 }
---
+++ src/org/gitPackage/TestPrivate.java
@@ -0,0 +1,28 @@
+package org.gitPackage;
+
+ class TestPrivate {
+
+	private int num=10;
+	
+	private void name() {
+		
+		System.out.println("helo java");
+		
+	}
+	
+	
+}
+
+ class Mai{
+	 
+	 public static void main(String[] args) {
+		
+		 TestPrivate tp = new TestPrivate();
+		 
+		
+		 
+		 
+	}
+	 
+	 
+ }
